f8f2d0cc4261804a00c22290d2cba5525b116dec
Jason Valenzuela <jvalenzuela1977@gmail.com>
2025-08-12 15:43:53 -0400

    Add configuration to compile into an exe.

--- diff/__main__.py
+++
@@ -1,7 +0,0 @@
-"""Top-level entry point for running the package."""
-
-from . import gui
-
-
-if __name__ == "__main__":
-    gui.run()
---
+++ main.py
@@ -0,0 +1,5 @@
+"""Top-level entry point for running the package."""
+
+from diff import gui
+
+gui.run()
